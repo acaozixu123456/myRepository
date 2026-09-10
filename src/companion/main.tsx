9b7014f5e58e65d8e818afbe15058118d1de1034
acaozixu123456 <331004814@qq.com>
2026-09-11 08:41:08 +0900

    Use the complete neon stylesheet without legacy light-theme important overrides

--- src/companion/main.tsx
+++ src/companion/main.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import CompanionApp from './CompanionApp';
-import './companion.css';
-import './refinements.css';
 import './neon.css';
 createRoot(document.getElementById('companion-root')!).render(<React.StrictMode><CompanionApp/></React.StrictMode>);
