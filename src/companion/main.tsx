05d1585a3b0d9f24a316c1d5a4bbfde84ae4d0c7
acaozixu123456 <331004814@qq.com>
2026-09-11 08:49:02 +0900

    Activate screenshot-reviewed high-contrast written support styling

--- src/companion/main.tsx
+++ src/companion/main.tsx
@@ -2,4 +2,5 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import CompanionApp from './CompanionApp';
 import './neon.css';
+import './neonLegibility.css';
 createRoot(document.getElementById('companion-root')!).render(<React.StrictMode><CompanionApp/></React.StrictMode>);
